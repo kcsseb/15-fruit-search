d337566e982eefde4e4dc5b388676848083ad2cd
kcsseb <kcsseb332@gmail.com>
2024-06-09 23:31:46 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+const fruits = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+	let results = fruits.filter((fruit) => (fruit.toLowerCase()).indexOf(str) >= 0);
+	return results;
+}
+
+function searchHandler(e) {
+	suggestions.innerHTML = "";
+	let results = search(e.target.value.toLowerCase());
+	showSuggestions(results);
+}
+
+function showSuggestions(results) {
+	for (let result of results) {
+		let item = document.createElement('li');
+		item.innerText = result;
+		suggestions.appendChild(item);
+	}
+}
+
+function useSuggestion(e) {
+	input.value = e.target.innerText;
+	suggestions.innerHTML = "";
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
